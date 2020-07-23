5d3868e155bbefbe5f277da088aa69d9d345faee
nikkilkum26 <52193968+nikkilkum26@users.noreply.github.com>
2020-07-23 16:45:23 +0530

    Add files via upload

---
+++ Week_1/day_1/maxarrays.js
@@ -0,0 +1,9 @@
+let a=[1,4,9,36,78,60,32,21,12];
+let max=0;
+for (let i= 0; i<a.length ;i++)
+{
+	if (a[i]>max)
+	max= a[i];
+}
+
+console.log(max);
---
+++ Week_1/day_1/sumarray.js
@@ -0,0 +1,8 @@
+let a=[1,4,9,36,78,60,32,21,12];
+let sum=0;
+for (let i= 0; i<a.length ;i++)
+{
+	sum +=a[i];
+}
+
+console.log(sum);
