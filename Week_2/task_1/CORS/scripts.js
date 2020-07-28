779405b553f2216bd42ef31faceb8e2665daf894
Nikkil kumar <52193968+nikkilkum26@users.noreply.github.com>
2020-07-28 17:51:02 +0530

    Update scripts.js

--- Week_2/task_1/CORS/scripts.js
+++ Week_2/task_1/CORS/scripts.js
@@ -1,8 +1,11 @@
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest()
-var url_string = 'https://api.domainsdb.info/v1/domains/search?domain=facebook&zone=com';
+
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET',url_string , true)
+let url_string = 'https://api.domainsdb.info/v1/domains/search?domain=facebook&zone=com';
+let proxy = 'https://cors-anywhere.herokuapp.com/'
+request.open('GET', proxy+url_string, true)
+//request.setRequestHeader('Access-Control-Allow-Origin', 'www.google.com');
 request.send();
 request.onload = function() {
   // Begin accessing JSON data here
@@ -11,7 +14,7 @@ console.log(data);
 }
 
 /*
-fetch(url_string) // Call the fetch function passing the url of the API as a parameter
+fetch(url_string)
 .then(res=>res.json())
 .then(data=>console.log(data))
 */
