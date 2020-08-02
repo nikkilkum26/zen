a4d5ee2b73ec0417688b7128becd35a093e608b4
Nikkil kumar <52193968+nikkilkum26@users.noreply.github.com>
2020-08-02 20:53:25 +0530

    Update anonymous_function_3.js

--- Week_2/task4/anonymous_function/anonymous_function_3.js
+++ Week_2/task4/anonymous_function/anonymous_function_3.js
@@ -3,7 +3,7 @@ let anon=[1,2,3,4,5];
 ((anon)=>{
     let b=[];
     for(let i=0;i<anon.length;i++){
-        b= anon[i]*anon[i];
+        b[i]= anon[i]*anon[i];
     }
     console.log(b);
 })(anon);
