c34a8ba84cb9ba51a24502738fba3ea60cf6e2d4
Nikkil kumar <52193968+nikkilkum26@users.noreply.github.com>
2020-08-18 10:06:48 +0530

    Update script.js

--- week_5/17-08-20/CALL BACK HELL TASK/script.js
+++ week_5/17-08-20/CALL BACK HELL TASK/script.js
@@ -1,16 +1,51 @@
 let div=document.createElement('div');
 div.setAttribute('id','value');
 document.body.append(div);
-function countDown(s) {
-let callBack = document.getElementById("value");
-callBack.innerHTML = "Please wait for "+s+" seconds";
-if(s < 1) {
-    clearTimeout(timer);
-    callBack.innerHTML = '<h1>Happy Independence day</h1>';
-    
-}
-s--;
- timer = setTimeout('countDown('+s+',"'+value+'")',1000);
-}
-countDown(10);
 
+// recursion
+// function countDown(s) {
+// let callBack = document.getElementById("value");
+// callBack.innerHTML = "Please wait for "+s+" seconds";
+// if(s < 1) {
+//     clearTimeout(timer);
+//     callBack.innerHTML = '<div>Happy Independence day</div>';
+    
+// }
+// s--;
+//  timer = setTimeout('countDown('+s+',"'+value+'")',1000);
+// }
+// countDown(10);
+
+setTimeout(function(){ 
+    setTimeout(function(){
+        setTimeout(function(){
+             setTimeout(function(){
+                  setTimeout(function(){
+                      setTimeout(function(){
+                          setTimeout(function(){
+                              setTimeout(function(){
+                                  setTimeout(function(){
+                                      setTimeout(function(){
+                                         setTimeout(function(){
+                                            div.innerHTML = '<h1>Happy Independence day</h1>';
+                                         });
+                                         div.innerText="1";
+                                      },1000)
+                                      div.innerText="2";
+                                  },1000)
+                                  div.innerText="3";
+                              },1000)
+                              div.innerText="4";
+                          },1000)
+                          div.innerText="5";
+                      },1000)
+                      div.innerText="6";
+                  },1000);
+                  div.innerText="7";
+             },1000);
+             div.innerText="8";
+        },1000);
+        div.innerText="9";
+    },1000);
+    div.innerText="10";
+}, 1000);
