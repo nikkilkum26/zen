1d5037044795b8a8af7d52098b6fefd076fcd6c0
Nikkil kumar <52193968+nikkilkum26@users.noreply.github.com>
2020-08-18 12:03:54 +0530

    Update script.js

--- week_5/17-08-20/CALL BACK HELL TASK/script.js
+++ week_5/17-08-20/CALL BACK HELL TASK/script.js
@@ -15,37 +15,37 @@ document.body.append(div);
 //  timer = setTimeout('countDown('+s+',"'+value+'")',1000);
 // }
 // countDown(10);
-
+//without recursion
 setTimeout(function(){ 
-    setTimeout(function(){
-        setTimeout(function(){
-             setTimeout(function(){
-                  setTimeout(function(){
-                      setTimeout(function(){
-                          setTimeout(function(){
-                              setTimeout(function(){
-                                  setTimeout(function(){
-                                      setTimeout(function(){
-                                         setTimeout(function(){
-                                            div.innerHTML = '<h1>Happy Independence day</h1>';
-                                         });
-                                         div.innerText="1";
-                                      },1000)
-                                      div.innerText="2";
-                                  },1000)
-                                  div.innerText="3";
-                              },1000)
-                              div.innerText="4";
-                          },1000)
-                          div.innerText="5";
-                      },1000)
-                      div.innerText="6";
-                  },1000);
-                  div.innerText="7";
-             },1000);
-             div.innerText="8";
-        },1000);
-        div.innerText="9";
-    },1000);
-    div.innerText="10";
+setTimeout(function(){
+setTimeout(function(){
+setTimeout(function(){
+setTimeout(function(){
+setTimeout(function(){
+setTimeout(function(){
+setTimeout(function(){
+setTimeout(function(){
+setTimeout(function(){
+setTimeout(function(){
+div.innerHTML = '<h1>Happy Independence day</h1>';
+ });
+ div.innerText="Please wait for 1 seconds";
+},1000)
+div.innerText="Please wait for 2 seconds";
+},1000)
+div.innerText="Please wait for 3 seconds";
+ },1000)
+div.innerText="Please wait for 4 seconds";
+},1000)
+div.innerText="Please wait for 5 seconds";
+},1000)
+div.innerText="Please wait for 6 seconds";
+ },1000);
+div.innerText="Please wait for 7 seconds";
+},1000);
+div.innerText="Please wait for 8 seconds";
+},1000);
+div.innerText="Please wait for 9 seconds";
+},1000);
+div.innerText="Please wait for 10 seconds";
 }, 1000);
